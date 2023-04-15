3632cf591719822ac7fead50ccec6d1b67144441
Nir Elbaz <nire0510@gmail.com>
2023-04-15 18:24:09 +0300

    Downgrade target to es6

--- build/tests/unit/utils-fs.test.js
+++ build/tests/unit/utils-fs.test.js
@@ -22,6 +22,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -29,14 +38,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const os_1 = __importDefault(require("os"));
 const fsUtils = __importStar(require("../../src/utils/fs"));
 describe('FS Utility', () => {
-    test('should generate random file name in temp folder', async () => {
+    test('should generate random file name in temp folder', () => __awaiter(void 0, void 0, void 0, function* () {
         const filepath = fsUtils.generateTempFilePath('png');
         const tmpdir = os_1.default.tmpdir();
         expect(filepath.indexOf(tmpdir) === 0).toBeTruthy();
-    });
-    test('should generate random file name with specified file extension', async () => {
+    }));
+    test('should generate random file name with specified file extension', () => __awaiter(void 0, void 0, void 0, function* () {
         expect(fsUtils.generateTempFilePath('png').endsWith('.png')).toBeTruthy();
         expect(fsUtils.generateTempFilePath('pdf').endsWith('.pdf')).toBeTruthy();
         expect(fsUtils.generateTempFilePath('txt').endsWith('.txt')).toBeTruthy();
-    });
+    }));
 });
