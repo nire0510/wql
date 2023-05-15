21a5397f12585a997ab6f2ca406d04fe9735f283
Nir Elbaz <nire0510@gmail.com>
2023-05-15 16:19:04 +0300

    Move types & index into src

--- build/src/index.js
+++ build/src/index.js
@@ -2,7 +2,7 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-const main_1 = __importDefault(require("./src/modules/main"));
+const main_1 = __importDefault(require("./modules/main"));
 module.exports = function wql(query, options = {}) {
     return (0, main_1.default)(query, options);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import Response from './src/models/response';
-import run from './src/modules/main';
+import Response from './models/response';
+import run from './modules/main';
 
 export = function wql(query: string, options = {} as Options): Promise<Response[]> {
   return run(query, options);
