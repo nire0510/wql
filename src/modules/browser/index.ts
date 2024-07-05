2ffe4427b0b3df604bd16274bb9567ed530e7302
Nir Elbaz <nir@splittytravel.com>
2024-07-05 13:40:46 +0300

    Disable AdblockerPlugin

--- src/modules/browser/index.ts
+++ src/modules/browser/index.ts
@@ -4,8 +4,8 @@ import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker';
 
-puppeteer.use(StealthPlugin())
-puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
+puppeteer.use(StealthPlugin());
+// puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
 
 export default class Browser {
   browser: PuppeteerBrowser | null;
